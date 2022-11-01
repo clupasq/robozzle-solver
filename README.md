40461b65b4ea96119ebb3e80aca959f2219703e0
Cristian Lupascu <cristi.lupascu@pm.me>
2022-11-01 12:28:29 +0200

    Level downloader

---
+++ README.md
@@ -0,0 +1,4 @@
+# Robozzle Solver
+
+A downloader and (hopefully) solver for http://www.robozzle.com/js/index.aspx puzzles.
+
---
+++ download.js
@@ -0,0 +1,34 @@
+const axios = require("axios")
+
+const downloadLevel = async (levelNo) => {
+    const response = await axios({
+        method: "POST",
+        url: "http://www.robozzle.com/RobozzleService.svc",
+        data: `<?xml version="1.0" encoding="UTF-8"?>
+        <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <soap:Body>
+        <GetLevel xmlns="http://tempuri.org/">
+            <levelId>${levelNo}</levelId>
+        </GetLevel>
+    </soap:Body>
+</soap:Envelope>
+`,
+        responseType: "text",
+        headers: {
+            "Content-Type": "text/xml; charset=UTF-8",
+            "SOAPAction": "http://tempuri.org/IRobozzleService/GetLevel",
+        }
+    })
+
+    return response.data
+}
+
+const main = async () => {
+    console.log(await downloadLevel(101))
+}
+
+
+main()
+    .then(() => console.log("Done"))
+    .catch(e => console.error(e.message))
+
